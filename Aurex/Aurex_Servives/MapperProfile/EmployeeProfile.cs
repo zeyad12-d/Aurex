5d427c8173fd7718c7c7c94ecd46528df582f648
zeyad12-d <zeadyasser054@gmail.com>
2025-10-12 23:29:04 +0300

    feat:EmployeeProfile

---
+++ Aurex/Aurex_Servives/MapperProfile/EmployeeProfile.cs
@@ -0,0 +1,20 @@
+﻿using Aurex_Core.DTO.EmployeeDtos;
+using Aurex_Core.Entites;
+using AutoMapper;
+
+namespace Aurex_Services.MapperProfile
+{
+    public sealed class EmployeeProfile :Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<Employee,EmployeeResponseDto>()
+                .ForMember(dest => dest.DepartmentName, opt => opt.MapFrom(src => src.Department.Name !=null ? src .Department.Name:null))
+                .ReverseMap(); 
+            CreateMap<CreateEmployeeDto, Employee>().ReverseMap();
+
+            CreateMap<UpdateEmployeeDto, Employee>().ReverseMap();
+
+        }
+    }
+}
