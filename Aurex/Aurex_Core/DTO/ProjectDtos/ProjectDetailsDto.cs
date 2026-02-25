874001e950e96c1ce10dd4dac66ebfe92e194699
zeyad12-d <zeadyasser054@gmail.com>
2026-02-25 16:54:27 +0200

    add-ProjectDetailsDto

---
+++ Aurex/Aurex_Core/DTO/ProjectDtos/ProjectDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Aurex_Core.DTO.ProjectDtos
+{
+    public record ProjectDetailsDto
+    {
+        public DateTime DeadlineDate { get; set; }
+        public string Description { get; set; } = String.Empty;
+
+        public string Name { get; set; } = String.Empty;
+
+        public string? ImageUrl { get; set; } 
+    }
+}
