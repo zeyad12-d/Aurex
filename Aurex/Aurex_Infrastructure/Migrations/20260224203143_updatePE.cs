e973c2301b70856f967c5aae25086e7af8273b2b
zeyad12-d <zeadyasser054@gmail.com>
2026-02-25 16:51:36 +0200

    update

---
+++ Aurex/Aurex_Infrastructure/Migrations/20260224203143_updatePE.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aurex_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatePE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Projects");
+        }
+    }
+}
