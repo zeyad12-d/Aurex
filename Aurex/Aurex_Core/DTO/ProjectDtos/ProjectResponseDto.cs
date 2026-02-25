c9bf0b3d7544112c0ec8eea5e808d898c86396f8
zeyad12-d <zeadyasser054@gmail.com>
2026-02-25 16:50:36 +0200

    add-projectResponseDto

---
+++ Aurex/Aurex_Core/DTO/ProjectDtos/ProjectResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using Aurex_Core.Entites;
+
+namespace Aurex_Core.DTO.ProjectDtos
+{
+    public record ProjectResponseDto
+    {
+        public decimal Amount { get; set; }
+        public int Probability { get; set; }
+        public DateTime DeadlineDate { get; set; }
+        public ProjectStatus Status { get; set; }
+        public NegotiationStage Negotiation { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
