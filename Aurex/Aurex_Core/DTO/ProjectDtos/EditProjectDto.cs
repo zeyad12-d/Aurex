aca74b642b16ed9271f53528c1dfe8bebebb2508
zeyad12-d <zeadyasser054@gmail.com>
2026-02-25 17:14:02 +0200

    add-EditprojectDto

---
+++ Aurex/Aurex_Core/DTO/ProjectDtos/EditProjectDto.cs
@@ -0,0 +1,24 @@
+﻿using Aurex_Core.Entites;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aurex_Core.DTO.ProjectDtos
+{
+    public record EditProjectDto
+    {
+        
+        public string? Description { get; set; } = String.Empty;
+       
+        public string? Name { get; set; } = String.Empty;
+        
+        public decimal? Amount { get; set; }
+     
+        public int? Probability { get; set; }
+
+       
+        public DateTime? DeadlineDate { get; set; }
+       
+        public ProjectStatus? Status { get; set; }
+      
+        public NegotiationStage? Negotiation { get; set; }
+    }
+}
